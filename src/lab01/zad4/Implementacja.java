e49711476dd3100f4bb4f0118bf9c7d000d63e93
Jedrek <jedrzej.jakubowski1@gmail.com>
2016-10-16 21:39:53 +0200

    add HultajchochlaTest

--- src/lab01/zad4/Implementacja.java
+++ src/lab01/zad4/Implementacja.java
@@ -1,5 +1,9 @@
 package lab01.zad4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Implementacja implements IPsikus {
@@ -32,11 +36,42 @@ public class Implementacja implements IPsikus {
 		}
 		return result;
 	}
+	
+	public int getDiffIndex(int pierwszyIndex, int size, Random generator){
+//		System.out.println(size);
+		int result = generator.nextInt(size);
+		if (result == pierwszyIndex){
+			return getDiffIndex(pierwszyIndex, size, generator);
+		} else {
+			return result;
+		}
+	}
 
 	@Override
 	public Integer hultajchochla(Integer liczba) throws NieudanyPsikusException {
-		// TODO Auto-generated method stub
-		return null;
+//		System.out.println(liczba);
+		if (Integer.toString(liczba).length() == 1){
+			throw new NieudanyPsikusException();
+		} else {
+			String liczbaString = Integer.toString(liczba);
+			List<Integer> liczbaArray = new ArrayList<Integer>();
+			for (int i = 0; i < liczbaString.length(); i++){
+				liczbaArray.add(Character.getNumericValue(liczbaString.charAt(i)));
+			}
+			
+			Random generator = new Random();
+			int pierwszyIndex = generator.nextInt(liczbaArray.size());
+			int drugiIndex = getDiffIndex(pierwszyIndex, liczbaArray.size(), generator);
+			
+			Collections.swap(liczbaArray, pierwszyIndex, drugiIndex);
+			StringBuilder strBuilder = new StringBuilder();
+			for(int i = 0; i < liczbaArray.size(); i++){
+				strBuilder.append(liczbaArray.get(i).toString());
+			}
+//			System.out.println(Integer.parseInt(strBuilder.toString()));
+			return Integer.parseInt(strBuilder.toString());
+			
+		}
 	}
 
 	@Override
