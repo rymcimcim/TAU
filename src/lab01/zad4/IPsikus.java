a8b902d1fc0bfb0b45107bd41fa01f1b63caf490
Jędrzej Jakubowski <jedrzej.jakubowski1@gmail.com>
2016-10-23 10:52:43 +0200

    Delete IPsikus.java

--- src/lab01/zad4/IPsikus.java
+++
@@ -1,7 +0,0 @@
-package lab01.zad4;
-
-public interface IPsikus {
-	Integer cyfrokrad(Integer liczba);
-	Integer hultajchochla(Integer liczba) throws NieudanyPsikusException;
-	Integer nieksztaltek(Integer liczba);
-}
