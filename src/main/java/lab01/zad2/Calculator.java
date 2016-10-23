b4cb3387b1cfc5c011822ee93124aefeac7d395b
s11869 <s11869@lab5l-8.pjwstk.edu.pl>
2016-10-23 11:36:46 +0200

    refactor v2

---
+++ src/main/java/lab01/zad2/Calculator.java
@@ -0,0 +1,31 @@
+package lab01.zad2;
+
+
+
+public class Calculator {
+	public Calculator(){}
+	
+	double add(double a, double b){
+		return a + b;
+	}
+	
+	double sub(double a, double b){
+		return a - b;
+	}
+	double multi(double a, double b){
+		return a * b;
+	}
+	double div(double a, double b){
+		double div;
+		try{
+			div = a / b;
+		}
+		catch(ArithmeticException e){
+			div = 0;
+		}
+		return div;
+	}
+	boolean greater(double a, double b){
+		return a > b;
+	}
+}
---
+++ src/main/java/lab01/zad4/IPsikus.java
@@ -0,0 +1,7 @@
+package lab01.zad4;
+
+public interface IPsikus {
+	Integer cyfrokrad(Integer liczba);
+	Integer hultajchochla(Integer liczba) throws NieudanyPsikusException;
+	Integer nieksztaltek(Integer liczba);
+}
